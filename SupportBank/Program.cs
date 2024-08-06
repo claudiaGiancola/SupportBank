875fd8e326f086e596aff494247c2ebd1f4b2f01
claudiaGiancola <cloemezzaluna@gmail.com>
2024-08-06 15:42:48 +0100

    refactoring + List [Account] - prints out every transaction (with date, narrative, to, from and amount) for a specific account the user

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -3,58 +3,114 @@ using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 
-List<Transaction> transactionsList = new List<Transaction>();
-
-// Open the file using a StreamReader
-using (var reader = new StreamReader("C:/Training/w6d2_SupportBank/Transactions2014.csv"))
+List<Transaction> getTransactionsListFromCSV(string csvPath)
 {
-    // Read the first line of the file
-    var headerLine = reader.ReadLine();
-    string line;
+    List<Transaction> transactionsList = new List<Transaction>();
 
-    // Read the rest of the file
-    while ((line = reader.ReadLine()) != null)
+    // Open the file using a StreamReader
+    using (var reader = new StreamReader(csvPath))
     {
+        // Read the first line of the file
+        var headerLine = reader.ReadLine();
+        string line;
 
-        // Split the data line into an array of values
-        var values = line.Split(',');
-
-        Transaction transaction = new Transaction();
+        // Read the rest of the file
+        while ((line = reader.ReadLine()) != null)
         {
-            transaction.date = values[0];
-            transaction.from = values[1];
-            transaction.to = values[2];
-            transaction.narrative = values[3];
-            transaction.amount = float.Parse(values[4]);
-        };
-        transactionsList.Add(transaction);
 
-        //code below tests content of each transaction instance
-        // Console.WriteLine(transaction.date + " " + transaction.from + " " + transaction.to + " " + transaction.narrative + " " + transaction.amount);
+            // Split the data line into an array of values
+            var values = line.Split(',');
+
+            Transaction transaction = new Transaction();
+            {
+                transaction.date = values[0];
+                transaction.from = values[1];
+                transaction.to = values[2];
+                transaction.narrative = values[3];
+                transaction.amount = float.Parse(values[4]);
+            };
+            transactionsList.Add(transaction);
+
+            //code below tests content of each transaction instance
+            // Console.WriteLine(transaction.date + " " + transaction.from + " " + transaction.to + " " + transaction.narrative + " " + transaction.amount);
+        }
     }
+    return transactionsList;
 }
 
-List<string> userFromList = transactionsList.Select(x => x.from).Distinct().ToList();
-List<string> userToList = transactionsList.Select(x => x.to).Distinct().ToList();
-
-List<string> uniqueUsers = userFromList.Union(userToList).ToList();
-
-List<Account> accounts = new List<Account>();
-
-foreach (var user in uniqueUsers)
+List<Account> getAccounts(List<Transaction> transactionsList)
 {
-    Account account = new Account();
+    List<string> userFromList = transactionsList.Select(x => x.from).Distinct().ToList();
+    List<string> userToList = transactionsList.Select(x => x.to).Distinct().ToList();
 
+    List<string> uniqueUsers = userFromList.Union(userToList).ToList();
+
+    List<Account> accounts = new List<Account>();
+
+    foreach (var user in uniqueUsers)
     {
-        account.name = user;
+        Account account = new Account();
 
-        account.MoneyBorrowed = account.getTransactionsBorrowed(transactionsList).Select(x => x.amount).Sum();
+        {
+            account.name = user;
 
-        account.MoneyLent = account.getTransactionsLent(transactionsList).Select(x => x.amount).Sum();
+            account.MoneyBorrowed = account.getTransactionsBorrowed(transactionsList).Select(x => x.amount).Sum();
 
-        accounts.Add(account);
-        
-    };
+            account.MoneyLent = account.getTransactionsLent(transactionsList).Select(x => x.amount).Sum();
 
-    Console.WriteLine($"{account.name} has borrowed {account.MoneyBorrowed} and lent {account.MoneyLent}");
+            accounts.Add(account);
+            //Console.WriteLine($"{account.name} has borrowed {account.MoneyBorrowed} and lent {account.MoneyLent}");
+
+        };
+
+    }
+    return accounts;
 }
+
+void listAll(List<Account> accounts)
+{
+
+    foreach (var account in accounts)
+    {
+        Console.WriteLine($"{account.name} has borrowed {account.MoneyBorrowed} and lent {account.MoneyLent}");
+    };
+}
+
+void listUser(List<Account> accounts, string accountName, List<Transaction> transactionsList)
+{
+    //filter accounts with accountName
+    List<Account> thisAccount = accounts.Where(p => p.name == accountName).ToList();
+    Account account = thisAccount[0];
+    
+    Console.WriteLine($"{account.name} has borrowed {account.MoneyBorrowed} and lent {account.MoneyLent}");
+    Console.WriteLine($"List of {account.name}'s transactions:");
+
+    Console.WriteLine($"{account.name} borrowed:");
+    foreach (var transaction in account.getTransactionsLent(transactionsList)) {
+        Console.WriteLine(transaction.date + " " + transaction.from + " " + transaction.to + " " + transaction.narrative + " " + transaction.amount);
+    }
+
+    Console.WriteLine($"{account.name} lent:");
+    foreach (var transaction in account.getTransactionsBorrowed(transactionsList)) {
+        Console.WriteLine(transaction.date + " " + transaction.from + " " + transaction.to + " " + transaction.narrative + " " + transaction.amount);
+    }
+
+}
+
+void useSupportBank()
+{
+
+    //readFile and create transactions instances
+    List<Transaction> transactionsList = getTransactionsListFromCSV("C:/Training/w6d2_SupportBank/Transactions2014.csv");
+
+    //from transactions create accounts
+    List<Account> accounts = getAccounts(transactionsList);
+
+    
+    //user selects if printing all results or results for a specific user
+    listAll(accounts);
+
+    listUser(accounts, "Jon A", transactionsList);
+}
+
+useSupportBank();
--- SupportBank/obj/Debug/net8.0/SupportBank.AssemblyInfo.cs
+++ SupportBank/obj/Debug/net8.0/SupportBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupportBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbab79466978b03589efae8dfd0c2fe6f796c48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e748fc20a0061da43cf8b4baddfd3ec613b3f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupportBank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupportBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
